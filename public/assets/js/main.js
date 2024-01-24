cfdc237be63b92b177ccf5afb566fccb8d43e86a
Ardiansyah <120023934+bambangardiansyah@users.noreply.github.com>
2024-01-24 08:43:30 +0700

    Add files via upload

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -17,6 +17,24 @@
     });
 
 
+    function toggleDropdown() {
+        var dropdown = document.getElementById("myDropdown");
+        dropdown.classList.toggle("show");
+      }
+      
+      window.onclick = function(event) {
+        if (!event.target.matches('.dropbtn')) {
+          var dropdowns = document.getElementsByClassName("dropdown-content");
+          for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+              openDropdown.classList.remove('show');
+            }
+          }
+        }
+      }
+      
+
     //Smoth Scroll
     $(function () {
         $('.nav-link, .smoth-scroll').on('click', function (event) {
